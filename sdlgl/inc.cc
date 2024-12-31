023e83382d084e698bb3b0b6b0b1ef0fd1a2f1be
jaburns <j@jaburns.net>
2024-12-30 18:32:50 -0700

    Pull out build stuff

---
+++ sdlgl/inc.cc
@@ -0,0 +1,7 @@
+#include "inc.hh"
+
+#include "../base/inc.cc"
+
+// #include "gfx.cc"
+// #include "audio.cc"
+// #include "window.cc"
---
+++ sdlgl/inc.hh
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <GL/glew.h>
+#include <SDL2/SDL.h>
+
+// #include "../vendor/cimgui/imgui_impl.h"
+#include "../vendor/minivorbis.h"
+#include "../vendor/stb_image.h"
+
+#include "../base/inc.hh"
+
+// #include "gfx.hh"
+// #include "audio.hh"
+// #include "window.hh"
